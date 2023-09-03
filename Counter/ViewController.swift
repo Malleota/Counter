1abe18a0449644bd8c26e9820073362782ad447a
Daria <zharko.daria@mail.ru>
2023-09-03 17:11:54 +0300

    First Variant

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countButtin: UIButton!
+    
+    @IBOutlet weak var countLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var countValue: Int = 0
+    @IBAction func clickCountButton(_ sender: Any) {
+        countValue += 1
+        countLable.text = String(countValue)
+        print(countValue)
+    }
+    
 
 }
 
